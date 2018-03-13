e678565bad6d3d97d435c97a172abbbd7f47c0f8
Adanna Omatu <adaomatu@gmail.com>
2018-03-13 16:34:26 -0400

    created db

---
+++ bird-app/db/birds.js
@@ -0,0 +1,51 @@
+const birds = [
+  {
+    id: 1,
+    type: 'Pigeon',
+    city: 'Seattle',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 2,
+    type: 'Sparrow',
+    city: 'NYC',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 3,
+    type: 'Bald Eagle',
+    city: 'Seattle',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 4,
+    type: 'Osprey',
+    city: 'NYC',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 5,
+    type: 'Hawk',
+    city: 'NYC',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 6,
+    type: 'Pigeon',
+    city: 'London',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 7,
+    type: 'Sparrow',
+    city: 'Austin',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    id: 8,
+    type: 'Canary',
+    city: 'NYC',
+    description: 'lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  }
+]
+module.exports = birds
